2c47bdeab9cd0bbb69c190ec20ac8f18246075dd
mary <mengistumary60@gmail.com>
2024-08-13 13:30:34 -0700

    t-0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+"""Write a function that queries the Reddit API and
+   returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """A function that returns the number of subscribers for a
+    given subreddit"""
+    user_agent = "0-subs/1.0"
+    headers = {"User-Agent": user_agent}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
+        subscribers = data["data"]["subscribers"]
         return subscribers
-    else:
+    elif response.status_code == 404:
         return 0
+    else:
+        print(f"Error: {response.status_code}")
