51dd664685d81aebcda42f34fe2af793ca8223c7
mary <mengistumary60@gmail.com>
2024-08-16 03:49:51 -0700

    t-0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# a puppet code that fixes a wordpress size 5xx error to 200 ok
+# editing the mistyped .phpp in the /var/www/html/wp-settings.php file
+
+exec { 'fix-wordpess-server-error':
+    command => 'sed -i s/phpp/php/g/var/www/html/wp-settings.php',
+    path    => '/user/bin/:/bin/',
+
+}
